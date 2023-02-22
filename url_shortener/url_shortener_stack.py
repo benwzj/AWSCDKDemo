ea0fcb610eac1078506c07078006e6e8f3560d8a
benwzj <57896457+benwzj@users.noreply.github.com>
2023-02-22 20:07:21 +1100

    first commit

--- url_shortener/url_shortener_stack.py
+++ url_shortener/url_shortener_stack.py
@@ -4,16 +4,11 @@ from aws_cdk import (
     # aws_sqs as sqs,
 )
 from constructs import Construct
+from . import widget_service
 
 class UrlShortenerStack(Stack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
-
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "UrlShortenerQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        widget_service.WidgetService(self, "Widgets")
